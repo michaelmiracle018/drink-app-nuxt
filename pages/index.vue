094fab9b635e7fd15f507864ca8442dd96760894
miraclemichael018 <miraclemichael018@gmail.com>
2022-11-22 14:34:04 +0000

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="main-content">
+    <Nav />
+    <Header />
+    <Employees v-for="(dataInfo,id) in employees" :key='id' :cardInfo="dataInfo"/>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Nav from '../components/Nav.vue'
+import Footer from '../components/Footer.vue'
+import Header from '../components/Header.vue'
+import Employees from '../components/Employees.vue'
+import employees from '../assets/data.js'
+export default {
+  name: 'IndexPage',
+  components: {
+    Nav,
+    Footer,
+    Header,
+    Employees,
+  },
+  data() {
+    return {
+       employees,
+    }
+  },
+  head() {
+    return {
+      title: 'Home',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Home View',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
